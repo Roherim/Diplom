ba85420cc80b52e2a467f38d451ac211eff55a8a
Roherim <64076990+Roherim@users.noreply.github.com>
2021-06-22 18:59:42 +0300

    Добавлено все

---
+++ Yasonov.site/Sourses/Scripts/scrolling.js
@@ -0,0 +1,11 @@
+function scrollingpaje(ell){
+window.scroll({
+	left: 0,
+	top: ell.offsetTop,
+	behavior: 'smooth'
+})
+}
+let Buttons=document.querySelector('.button');
+button.addEventListener('click', ()=>{
+	console.log("wefdsfsd")
+})
